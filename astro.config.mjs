6361a1350a77ae8cbbded39a83d4ac6a825ee71a
I Nengah Sugita <inengahsugita@Is-MacBook-Pro.local>
2026-09-24 12:02:14 +0800

    fix: integrate tailwind css and safe supabase dynamic routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'cloudflare',
   }),
+  integrations: [tailwind()],
 });
